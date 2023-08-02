7961ab0df2bec55e83d6a6db2a7adb9c6d406869
masnana <hariyadinana87@gmail.com>
2023-08-02 11:09:35 +0700

    update

--- components/Search.tsx
+++ components/Search.tsx
@@ -3,7 +3,7 @@ import { DocSearch } from '@docsearch/react';
 import '@docsearch/css';
 
 const Search: React.FC = () => {
-  return <DocSearch appId="8XXXVNJ7UW" apiKey="3680aeb79049744c6629dd0abd2f5dfe" indexName="overextended" />;
+  return <DocSearch appId="8XXXVNJ7UW" apiKey="3680aeb79049744c6629dd0abd2f5dfe" indexName="masdocs" />;
 };
 
 export default Search;
